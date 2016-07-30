7bcc1124c53e687427f16a6df23240ec5539d9f9
AradoBlitz <schlachtnavaho@gmail.com>
2016-07-30 11:48:44 +0300

    Implemented video recording and play feature.

--- src/video/audio/recorder/v2/VideoRecorder.java
+++ src/video/audio/recorder/v2/VideoRecorder.java
@@ -3,21 +3,78 @@ package video.audio.recorder.v2;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.github.sarxos.webcam.Webcam;
+import com.github.sarxos.webcam.WebcamEvent;
+import com.github.sarxos.webcam.WebcamListener;
+import com.github.sarxos.webcam.WebcamResolution;
+
+import video.audio.recorder.v2.video.Screen;
 
 public class VideoRecorder {
 
+	private Webcam webcam = Webcam.getDefault();
+
+	private Screen screen = new Screen();
+
 	public List<BufferedImage> video = new ArrayList<>();
 
+	public VideoRecorder() {
+		webcam.setViewSize(WebcamResolution.VGA.getSize());
+		WebcamListener camListener = new WebcamListener() {
+
+			@Override
+			public void webcamOpen(WebcamEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println(arg0.toString() + "webcamOpen");
+			}
+
+			@Override
+			public void webcamImageObtained(WebcamEvent arg0) {
+
+			}
+
+			@Override
+			public void webcamDisposed(WebcamEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println(arg0.toString() + "webcamDisposed");
+			}
+
+			@Override
+			public void webcamClosed(WebcamEvent arg0) {
+				// TODO Auto-generated method stub
+				System.out.println(arg0.toString() + "wbcamClosed");
+			}
+		};
+		webcam.addWebcamListener(camListener);
+	}
+
 	public void play() {
-		// TODO Auto-generated method stub
-		
+		for(BufferedImage image:video){
+			screen.setImage(image);
+			try {
+				TimeUnit.MILLISECONDS.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}	
+
 	}
 
 	public void record() {
-		// TODO Auto-generated method stub
-		
-	}
+		webcam.open();
+		try {
+			for(int i = 0;i<1000;i++){
+				BufferedImage image = webcam.getImage();
+				video.add(image);
+			}
+			
+		} finally {
+			webcam.close();
+		}
 
-	
+	}
 
 }
---
+++ src/video/audio/recorder/v2/video/Screen.java
@@ -0,0 +1,34 @@
+package video.audio.recorder.v2.video;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class Screen extends JPanel {
+
+	private Image image;
+
+	public Screen(){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);		
+		frame.add(this);	
+		frame.setSize(640, 480);
+		frame.setVisible(true);
+	}
+
+	public void setImage(Image image) {
+		this.image = image;
+		repaint();
+		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		g.drawImage(image, 0, 0, null);
+	}
+}
