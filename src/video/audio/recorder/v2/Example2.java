f33ba183aeb3c74bcd3dc1e717b4e29781efce16
AradoBlitz <schlachtnavaho@gmail.com>
2016-08-06 11:51:21 +0300

    Implemented counting sample for main thread.

--- src/video/audio/recorder/v2/Example2.java
+++ src/video/audio/recorder/v2/Example2.java
@@ -20,26 +20,34 @@ public class Example2 {
 	private List<BufferedImage> collectedImages = new ArrayList<>();
 	private List<byte[]> collectedSoundBytes = new ArrayList<>();
 	private VideoStreamRecorder videoStreamRecorder = new VideoStreamRecorder(recorderVideo);
-	private AudioStreamRecorder audioStreamRecorder = new AudioStreamRecorder(recorder); 
-	
-	
+	private AudioStreamRecorder audioStreamRecorder = new AudioStreamRecorder(recorder);
 
 	@Test
 	public void captureAudioAndVideo() throws Exception {
 
 		videoStreamRecorder.start();
 		audioStreamRecorder.start();
-		
+
 		TimeUnit.SECONDS.sleep(222);
-		
-		for(int i = 0;i<1000;i++){
+
+		for (int i = 0; i < 1000; i++) {
 			collectedImages.addAll(videoStreamRecorder.getCollectedImages());
 			collectedSoundBytes.add(audioStreamRecorder.getCollectedSound());
 		}
-	
-		assertEquals(1000,collectedSoundBytes.size());
-	assertEquals(1100,collectedImages.size());
-	
 
+		assertEquals(1000, collectedSoundBytes.size());
+		assertEquals(1100, collectedImages.size());
+
+	}
+	
+	@Test
+	public void countingSample() throws Exception {
+		int count=4;
+		assertEquals(0, count-4);
+		count+=5;
+		assertEquals(5, count-4);
+		count+=5;
+		assertEquals(10, count-4);
+		
 	}
 }
