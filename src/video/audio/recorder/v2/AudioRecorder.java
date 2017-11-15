516fb08b4c95ed83a597ebf63917ecb3efd79c7d
Dmitriy Vilyuzhanin <dmitriy.vilyuzhanin@gmail.com>
2017-11-16 02:49:50 +0300

    Introduced ring buffer to collect audio data.

--- src/video/audio/recorder/v2/AudioRecorder.java
+++ src/video/audio/recorder/v2/AudioRecorder.java
@@ -3,6 +3,7 @@ package video.audio.recorder.v2;
 import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
@@ -31,17 +32,52 @@ public class AudioRecorder {
 		}
 	}
 	
+
+
+	private volatile AudioItem[] rBuff = new AudioItem[frameCount];
+
+	private int buffIndex;
+	{
+		for(int i = 0; i<rBuff.length;i++)
+			rBuff[i]=new AudioItem();
+	}
+	
+	private static class AudioItem{
+
+		public long time;
+		public byte[] data;
+		
+	}
 	
 	public void record() throws Exception{
-		byte[] buff = new byte[1024];
-		int count = 0;		
-		for(int i = 0;i<frameCount;i++){
-			count = targetLine.read(buff, 0, buff.length);
-			time.add(System.currentTimeMillis());
-			ByteArrayOutputStream convertor = new ByteArrayOutputStream();
-			convertor.write(buff, 0, count);
-			audioCollector.add(convertor.toByteArray());
+		new Thread(){
+			
+			@Override
+				public void run() {
+					
+				byte[] buff = new byte[1024];
+				int count = 0;		
+				for(int i = 0;i<frameCount;i++){
+					count = targetLine.read(buff, 0, buff.length);
+					rBuff[buffIndex].time=System.currentTimeMillis();		
+					ByteArrayOutputStream convertor = new ByteArrayOutputStream();
+					convertor.write(buff, 0, count);
+					rBuff[buffIndex].data=convertor.toByteArray();
+					
+					buffIndex++;
+			
+				}
+			}
+		}.start();
+		
+		TimeUnit.SECONDS.sleep(10);
+		
+		for(int i =0;i<frameCount;i++){
+			time.add(rBuff[i].time);
+			audioCollector.add(rBuff[i].data);
 		}
+
+		
 
 		System.out.println("End audio recording. Collected " + audioCollector + "bytes.");
 	}
